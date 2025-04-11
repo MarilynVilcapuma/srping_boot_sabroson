678da480776a8da33f1929c9c709bba7a03ca032
Marilyn Vilcapuma Trujillo <marilyn.vilcapuma.trujillo@vallegrande.edu.pe>
2025-04-10 23:40:41 -0500

    feat: Add Spring Boot sabroson

---
+++ src/main/java/pe/edu/vallegrande/elsabroson/ElsabrosonApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.elsabroson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElsabrosonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElsabrosonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/vallegrande/elsabroson/ElsabrosonApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.elsabroson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElsabrosonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
